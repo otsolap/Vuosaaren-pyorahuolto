c90971bd27e653b9116cadb2ca7723a1254adc37
OtsoLap <otsolappalainen@gmail.com>
2022-08-18 07:26:36 +0300

    slow and steady

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import Links from '@content/site.json'
 import styles from '../../styles/Header.module.scss'
 
 
 const Header = () => {
-    const router = useRouter()
     const { Navigation } = Links
     return (
         <>
@@ -14,14 +12,14 @@ const Header = () => {
                 <div className={`container ${styles.navContainer}`}>
                     <div className={styles.logoContainer}>
                         <Link href="/"><a className={styles.noDecoration}>
-                            <h1 className={styles.logo}>{Navigation.logo}</h1>
+                            <h1 className={styles.logo}>{Navigation.titleLogo}</h1>
                         </a>
                         </Link>
-                        {Navigation.mobileImageLogo && (
-                            <div className={`mobile-only ${styles.mobileLogo}`}>
+                        {Navigation.imageLogo && (
+                            <div className={styles.logoImage}>
                                 <Image
-                                    src={Navigation.mobileImageLogo}
-                                    alt={Navigation.mobileImageAlt}
+                                    src={Navigation.imageLogo}
+                                    alt={Navigation.imageAlt}
                                     height={50}
                                     width={50}
                                     quality={100}
@@ -30,22 +28,6 @@ const Header = () => {
                                 />
                             </div>
                         )}
-                    </div>
-                    <div className={`desktop-only ${styles.desktopLinks}`}>
-                        <nav className={styles.nav}>
-                            <Link href="/">
-                                <a className={router.pathname == '/' ? styles.active : ''}
-                                >{Navigation.homeTitle}</a>
-                            </Link>
-                            <Link href="/juho">
-                                <a className={router.pathname == '/juho' ? styles.active : ''}
-                                >{Navigation.aboutMeTitle}</a>
-                            </Link>
-                            <Link href="/#yhteydenotto">
-                                <a className={router.pathname == '/#yhteydenotto' ? styles.active : ''}
-                                >{Navigation.contactTitle}</a>
-                            </Link>
-                        </nav>
                     </div>
                 </div>
             </header>
