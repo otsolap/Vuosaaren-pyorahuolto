927fbac8edb017569b942de472ddd84fe3f00e34
OtsoLap <otsolappalainen@gmail.com>
2022-08-26 21:19:44 +0300

    Salespage WIP

--- pages/polkupyorat.js
+++ pages/polkupyorat.js
@@ -1,12 +1,14 @@
 import Meta from "@components/Meta"
-import Hero from "@components/Hero"
-import ThemeBlock from "@components/salesPage/ImageAndText"
+import ImageAndText from "@components/salesPage/ImageAndText"
+import BenefitList from "@components/salesPage/BenefitList"
 
-const Polkupyorat = ({ meta, }) => {
+const Polkupyorat = ({ meta, blocks, benefits }) => {
     return (
         <>
             <Meta meta={meta} />
             <main id="polkupyorat">
+                <ImageAndText blocks={blocks} />
+                <BenefitList benefits={benefits} />
             </main>
         </>
     )
@@ -15,18 +17,22 @@ const Polkupyorat = ({ meta, }) => {
 export default Polkupyorat
 
 export async function getStaticProps() {
-    const sales = await import(`../content/sales.json`)
+    const sales = await import(`../content/polkupyorat.json`)
 
     return {
         props: {
+            blocks: {
+                blocks: sales.TextImageSection.blocks
+            },
+            benefits: {
+                sectionTitle: sales.BenefitsSection.SectionTitle,
+                benefits: sales.BenefitsSection.benefits
+            },
             meta: {
                 title: sales.meta.title,
                 description: sales.meta.description,
                 url: sales.meta.url,
                 image: sales.meta.image
-            },
-            blocks: {
-                blocks: sales.TextImageSection.blocks
             },
         }
     }
