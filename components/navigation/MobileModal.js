5a50d2c0bde6ea080e4c15522966bfbf0b04cb0b
OtsoLap <otsolappalainen@gmail.com>
2022-09-04 08:47:56 +0300

    added icons to mobile too

--- components/navigation/MobileModal.js
+++ components/navigation/MobileModal.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import Link from 'next/link'
 import Links from '@content/site.json'
 import { useRouter } from 'next/router'
+import { faNewspaper, faPersonSkiingNordic, faSnowflake, faScrewdriverWrench, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from '../../styles/MobileModal.module.scss'
 import useToggle from '@hooks/useToggleState'
 
@@ -20,16 +22,31 @@ const MobileModal = ({ show, closeModal }) => {
         closeModal()
     }
 
-    const MobileModalLinks = Links.Navigation.pages.slice(AFTER_PRIMARY_PAGES).map((link, i) => {
+    const MobileModalLinks = Links.Navigation.pages.slice(AFTER_PRIMARY_PAGES).map((page, i) => {
         return (
             <li
                 onClick={handleClick}
-                className={`${styles.linkWrapper} ${router.asPath === link.path ? styles.active : ''}`}
+                className={`${styles.linkWrapper} ${router.asPath === page.path ? styles.active : ''}`}
                 key={i}
             >
-                <Link href={link.path} >
+                <Link href={page.path} >
                     <a className={styles.link}>
-                        <span className={styles.mobileLinkText}>{link.title}</span>
+                        {page.icon === 'fixer' ? (
+                            <FontAwesomeIcon className={styles.sidebarIcon} aria-label={page.title} icon={faScrewdriverWrench} />
+                        ) : null}
+                        {page.icon === 'winter' ? (
+                            <FontAwesomeIcon className={styles.sidebarIcon} aria-label={page.title} icon={faSnowflake} />
+                        ) : null}
+                        {page.icon === 'skiis' ? (
+                            <FontAwesomeIcon className={styles.sidebarIcon} aria-label={page.title} icon={faPersonSkiingNordic} />
+                        ) : null}
+                        {page.icon === 'newspaper' ? (
+                            <FontAwesomeIcon className={styles.sidebarIcon} aria-label={page.title} icon={faNewspaper} />
+                        ) : null}
+                        {page.icon === 'contact' ? (
+                            <FontAwesomeIcon className={styles.sidebarIcon} aria-label={page.title} icon={faEnvelope} />
+                        ) : null}
+                        <span className={styles.mobileLinkText}>{page.title}</span>
                     </a>
                 </Link>
             </li>
